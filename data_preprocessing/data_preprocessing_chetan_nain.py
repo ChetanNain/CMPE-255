f4c6315f89da8765bf6a071d33d0bf9ae33adac9
Chetan Nain <chetan@Chetans-MacBook-Pro.local>
2022-05-22 17:17:27 -0700

    adding preprocessing and Memory based

--- data_preprocessing/data_preprocessing_chetan_nain.py
+++ data_preprocessing/data_preprocessing_chetan_nain.py
@@ -7,7 +7,7 @@
 
 # ### Importing the required libraries
 
-# In[3]:
+# In[154]:
 
 
 import pandas as pd
@@ -40,15 +40,9 @@ pd.set_option('display.max_rows',None)
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[4]:
-
-
-pip install urllib3
-
-
 # ### Loading the data
 
-# In[5]:
+# In[364]:
 
 
 books_data = pd.read_csv('../data/books_data.csv')
@@ -59,13 +53,13 @@ tags_data = pd.read_csv('../data/tags_data.csv')
 
 # ### Understanding the loaded data and checking shape
 
-# In[6]:
+# In[230]:
 
 
 books_data.head()
 
 
-# In[7]:
+# In[231]:
 
 
 books_data.shape
@@ -73,25 +67,25 @@ books_data.shape
 
 # ### Understanding the null values in the dataset
 
-# In[8]:
+# In[232]:
 
 
 books_data.isnull().sum()
 
 
-# In[12]:
+# In[233]:
 
 
 books_data['original_title'][books_data['isbn13']==9780451524940]
 
 
-# In[13]:
+# In[234]:
 
 
 books_data[books_data['isbn13']==9780451524940]
 
 
-# In[7]:
+# In[235]:
 
 
 msno.matrix(books_data)
@@ -101,27 +95,25 @@ msno.matrix(books_data)
 
 # Since isbn13 is more precise form of coding than isbn which is just a 10 digit number so with the presence of isbn13 we wont be needing isbn. So dropping isbn from the dataset.
 # 
-# Similarly, best_book_id is the book id of the latest version of the book available, so we feel that we wont be needing the book_id since we already have best_book_id.
-# 
 # Apart form that original title too is a redundant column because of the presence of title column.
 
-# In[8]:
+# In[236]:
 
 
-books_data = books_data.drop(['book_id', 'isbn','original_title'], axis=1)
+books_data = books_data.drop([ 'isbn','original_title'], axis=1)
 
 
 # ### Handling nulls in the dataset by imputing the values of language code (text data)
 
 # ### Now as we know, the column 'language_code' has a lot of null values. So we will try to impute these null values.
 
-# In[9]:
+# In[237]:
 
 
 nan_in_col  = books_data[books_data['language_code'].isna()]
 
 
-# In[10]:
+# In[238]:
 
 
 nan_in_col
@@ -132,7 +124,7 @@ nan_in_col
 # ### To solve this problem, lets take a small subset of data from the large dataset. 
 # ### In the below example I am taking the subset of data whose author is 'Charles Dickens'
 
-# In[11]:
+# In[239]:
 
 
 charlesDickens = books_data[books_data['authors']=='Charles Dickens']
@@ -145,7 +137,7 @@ charlesDickens
 # 
 # ### This way  we were able to retrieve around 50% of the missing values from the dataset.
 
-# In[12]:
+# In[240]:
 
 
 books_data['language_code'].fillna('unknown',inplace=True)
@@ -154,7 +146,7 @@ books_data['language_code'] = books_data.groupby('authors')['language_code'].tra
 
 # ### Now checking the data of Charles Dickens again we can see that the author of A Christmas Carol is imputed correctly
 
-# In[13]:
+# In[241]:
 
 
 charlesDickens = books_data[books_data['authors']=='Charles Dickens']
@@ -163,24 +155,43 @@ charlesDickens
 
 # ### Now checking the values that are still marked us 'unknown'
 
-# In[14]:
+# In[242]:
 
 
 unk = books_data[books_data['language_code']=='unknown']
 unk.shape
 
 
-# ### Since we have 584 values still marked as unknown, we will try and impute using the title of the book just in case there are some multiple entries of the same book
+# ### Since we have 584 values still marked as unknown, we will try and impute using the authors of the book just in case there are authors of similar books.
 
-# In[15]:
+# In[243]:
 
 
 books_data['language_code'] = books_data.groupby('title')['language_code'].transform(lambda x: x.replace('unknown',(x.mode()[0])))
 
 
+# In[244]:
+
+
+books_data['language_code'] = books_data.groupby('authors')['language_code'].transform(lambda x: x.replace('unknown',(x.mode()[0])))
+
+
+# In[245]:
+
+
+unk = books_data[books_data['language_code']=='unknown']
+unk.shape
+
+
+# In[246]:
+
+
+# books_data['language_code'] = books_data.groupby('isbn13')['language_code'].transform(lambda x: x.replace('unknown',(x.mode()[0])))
+
+
 # ### Now checking the shape of the dataset again
 
-# In[16]:
+# In[247]:
 
 
 unk = books_data[books_data['language_code']=='unknown']
@@ -189,13 +200,13 @@ unk.shape
 
 # ### Now imputing the same with the image url and checking the shape again
 
-# In[17]:
+# In[248]:
 
 
 books_data['language_code'] = books_data.groupby('image_url')['language_code'].transform(lambda x: x.replace('unknown',(x.mode()[0])))
 
 
-# In[18]:
+# In[249]:
 
 
 unk = books_data[books_data['language_code']=='unknown']
@@ -204,61 +215,76 @@ unk.shape
 
 # ### Now finally we were able to bring down the null values count to 267 from a count of 1000+
 
-# In[19]:
+# Dropping these unknown rows from the dataset and the rows which has isbn13 as null.
+
+# In[250]:
+
+
+books_data.drop(books_data[books_data['language_code'] == 'unknown'].index, inplace = True)
+
+
+# In[251]:
+
+
+books_data= books_data.dropna()
+books_data['isbn13'] = books_data['isbn13'].astype(int)
+
+
+# In[252]:
 
 
 msno.matrix(books_data)
 
 
-# In[20]:
+# In[253]:
 
 
 ratings_data.head()
 
 
-# In[21]:
+# In[254]:
 
 
 ratings_data.shape
 
 
-# In[22]:
+# In[255]:
 
 
 ratings_data.isnull().sum()
 
 
-# In[23]:
+# In[256]:
 
 
 book_tags_data.head()
 
 
-# In[24]:
+# In[257]:
 
 
 book_tags_data.shape
 
 
-# In[25]:
+# In[258]:
 
 
 book_tags_data.isnull().sum()
 
 
-# In[1]:
+# In[259]:
 
 
 tags_data
 
 
-# In[27]:
+# In[261]:
 
 
 tags_data.shape
 
 
-# In[28]:
+# In[262]:
 
 
 tags_data.isnull().sum()
@@ -270,7 +296,7 @@ tags_data.isnull().sum()
 
 # ### Lets start with counting the total number of books by every author
 
-# In[29]:
+# In[263]:
 
 
 books_data['authors'].value_counts()
@@ -280,7 +306,7 @@ books_data['authors'].value_counts()
 
 # ### The below script will help us picking up values where Dean Koontz is a co author as well.
 
-# In[30]:
+# In[264]:
 
 
 DKBooks = []
@@ -290,7 +316,7 @@ DKBooks
 
 # ### So as per the actual analysis, the total books published by Dean Koontz is 64
 
-# In[31]:
+# In[265]:
 
 
 len(DKBooks)
@@ -300,7 +326,7 @@ len(DKBooks)
 
 # ### Trying to figure out the actual number of books written by a specific author so that it can be included in analysis
 
-# In[32]:
+# In[266]:
 
 
 auth_books = []
@@ -308,7 +334,7 @@ books_data.authors.apply(lambda p: auth_books.append(p) if ',' in p else [])
 auth_books[:20]
 
 
-# In[33]:
+# In[267]:
 
 
 len(auth_books)
@@ -316,59 +342,53 @@ len(auth_books)
 
 # ### Splitting the data with comma separated values and then removing the actual author column and joining with the new splitted column
 
-# In[34]:
+# In[268]:
 
 
 splittedData = books_data['authors'].str.split(',').apply(Series, 1).stack()
 
 
-# In[35]:
+# In[269]:
 
 
 splittedData.index = splittedData.index.droplevel(-1)
 
 
-# In[36]:
+# In[270]:
 
 
 splittedData.name = 'authors'
 
 
-# In[37]:
+# In[271]:
 
 
 del books_data['authors']
 
 
-# In[38]:
+# In[272]:
 
 
 books_data=books_data.join(splittedData)
 
 
-# In[39]:
+# In[273]:
 
 
 books_data[books_data['best_book_id']==843]
 
 
-# In[40]:
+# In[274]:
 
 
 books_data.head()
-
-
-# In[ ]:
-
-
-
 
 
 # ### Now as we can see that the authors data in the dataset is cleaned and now we have authors column splitted into multiple rows so that we have only one author per row.
 # 
 # ### And this is proved by the following scripts.
 
-# In[41]:
+# In[275]:
 
 
 auth_books = []
@@ -376,7 +396,7 @@ books_data.authors.apply(lambda p: auth_books.append(p) if ',' in p else [])
 auth_books[:20]
 
 
-# In[42]:
+# In[276]:
 
 
 len(auth_books)
@@ -388,7 +408,7 @@ len(auth_books)
 
 # ### Drawing a box plot to understand the spread of average ratings.
 
-# In[43]:
+# In[210]:
 
 
 plt.figure(figsize=(8,4))
@@ -397,7 +417,7 @@ sns.boxplot(data=books_data['average_rating'])
 
 # ### Drawing a box plot for understanding the spread of publication year.
 
-# In[44]:
+# In[211]:
 
 
 plt.figure(figsize=(8,4))
@@ -406,7 +426,7 @@ sns.boxplot(data=books_data['original_publication_year'])
 
 # ### Drawing a scatter plot for understanding the distribution of ratings_5 vs average_ratings
 
-# In[45]:
+# In[212]:
 
 
 plt.figure(figsize=(28,10))
@@ -415,7 +435,7 @@ sns.lmplot(x='average_rating', y='ratings_5', data=books_data)
 
 # ### Sorting data according to the average ratings
 
-# In[46]:
+# In[213]:
 
 
 sortedData = books_data[books_data['ratings_count']>=1500]
@@ -423,7 +443,7 @@ sortedData = sortedData.sort_values('average_rating', ascending=False)
 sortedData.head()
 
 
-# In[47]:
+# In[214]:
 
 
 plt.figure(figsize=(15,12))
@@ -435,7 +455,7 @@ plt.ylabel('Book Title')
 
 # ## Count of books written in different languages
 
-# In[48]:
+# In[215]:
 
 
 langCounts = pd.DataFrame(books_data['language_code'].value_counts())
@@ -444,13 +464,13 @@ langCounts = langCounts.sort_values('Total Counts', ascending=False)
 langCounts
 
 
-# In[49]:
+# In[216]:
 
 
 len(langCounts)
 
 
-# In[50]:
+# In[217]:
 
 
 plt.figure(figsize=(16,8))
@@ -458,13 +478,13 @@ plt.title("Books Released Language Wise")
 plt.bar(x=langCounts.index,height='Total Counts', data=langCounts);
 
 
-# In[51]:
+# In[218]:
 
 
 langCounts = langCounts.drop(["en-US", "en-GB", "eng", "en-CA"])
 
 
-# In[52]:
+# In[219]:
 
 
 plt.figure(figsize=(16,8))
@@ -472,309 +492,121 @@ plt.title("Books Released Language Wise (Excluding English)")
 plt.bar(x=langCounts.index,height='Total Counts', data=langCounts);
 
 
-# In[53]:
-
-
-get_ipython().system('jupyter nbconvert data_pre*.ipynb --to python')
-
-
 # ## Adding Summary Dataset
 
-# In[54]:
+# In[277]:
 
 
 books_data.shape
 
 
-# In[56]:
+# In[278]:
 
 
-data = []
-
-with open("../data/booksummaries.txt", 'r',encoding='utf-8') as f:
-    reader = csv.reader(f, dialect='excel-tab')
-    for row in tqdm(reader):
-        data.append(row)
+books_data.head(2)
 
 
-# In[57]:
+# In[287]:
 
 
-book_id = []
-book_name = []
-summary = []
-genre = []
-
-for i in tqdm(data):
-    book_id.append(i[0])
-    book_name.append(i[2])
-    genre.append(i[5])
-    summary.append(i[6])
-
-books = pd.DataFrame({'book_id': book_id, 'book_name': book_name,
-                       'genre': genre, 'summary': summary})
-books.head(2)
+books_data['summary']=''
+books_data['genre']=''
+#HTTPError as err
 
 
-# In[58]:
+# In[351]:
 
 
-books.rename(columns = {'book_name':'title'}, inplace = True)
+while True:
+    try:
+        for index, row in books_data.iterrows():
+            if pd.isna(row['summary']):
+                print(row['title'])
+                time.sleep(0.5)
+                req = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + str(row['isbn13'])
+                res = urllib.request.urlopen(req)
+                result = json.loads(res.read().decode('utf-8'))
+                if 'items' in result:
+                    if 'volumeInfo' in result['items'][0]:
+                        if 'description' in result['items'][0]['volumeInfo']:
+                            print(row['title'])
+                            books_data.at[index , 'summary'] = result['items'][0]['volumeInfo']['description']
+                        if 'categories' in result['items'][0]['volumeInfo']:
+                            books_data.at[index , 'genre'] = result['items'][0]['volumeInfo']['categories'][0]
+                        else:
+                            books_data.at[index , 'summary']=np.nan
+                    else:
+                        books_data.at[index , 'summary']=np.nan
+        #                 row['summary']=result['items'][0]['volumeInfo']['description']
+                else:
+                    books_data.at[index , 'summary']=np.nan
+    except HTTPError as err:
+        print(err)
 
 
-# In[59]:
+# In[322]:
 
 
-books.drop(books[books['genre']==''].index, inplace=True)
-books[books['genre']=='']
+for col in books_data.columns:
+    print(col)
 
 
-# In[60]:
+# In[327]:
 
 
-genres = []
-for i in books['genre']:
-    genres.append(list(json.loads(i).values()))
-books['genre_new'] = genres
+books_data[books_data['summary']==''].shape
 
 
-# In[61]:
+# In[326]:
 
 
-all_genres = sum(genres,[])
-len(set(all_genres))
+books_data[books_data['summary']==np.nan].shape
 
 
-# In[62]:
+# In[356]:
 
 
+books_data.head(10)
+
+
+# In[374]:
+
+
+books_data = books_data.dropna()
+
+
+# In[375]:
+
+
+books_data = books_data[books_data['summary']!='']
+
+
+# In[376]:
+
+
+books_data.head(20)
+
+
+# In[377]:
+
+
+books_data.to_csv("cleaned_books_data.csv")
+
+
+# In[ ]:
+
+
+get_ipython().system('jupyter nbconvert data_pre*.ipynb --to python')
+
+
+# In[76]:
+
+
+# Extra function that can be used to clean summaries incase they are not clean.
 def clean_summary(text):
     text = re.sub("\'", "", text)
     text = re.sub("[^a-zA-Z]"," ",text)
     text = ' '.join(text.split())
     text = text.lower()
     return text
-
-
-# In[63]:
-
-
-# books_data['title'] = books_data['title'].apply(lambda x: clean_summary(x))
-books_data.head(2)
-
-
-# In[64]:
-
-
-books['clean_summary'] = books['summary'].apply(lambda x: clean_summary(x))
-books.head(2)
-
-
-# In[65]:
-
-
-books['title'] = books['title'].apply(lambda x: clean_summary(x))
-books.head(2)
-
-
-# In[ ]:
-
-
-
-
-
-# In[66]:
-
-
-merged_df=books_data.merge(books, on='title', how='left')
-
-
-# In[67]:
-
-
-merged_df.head(20)
-
-
-# In[68]:
-
-
-merged_df.shape
-
-
-# In[69]:
-
-
-merged_df.isnull().sum()
-
-
-# In[70]:
-
-
-books_data['summary']=''
-
-
-# In[71]:
-
-
-dummy_data=books_data.head(10)
-
-
-# In[72]:
-
-
-# split = dummy_data['isbn13'].str.split(',', 1, expand=True)
-dummy_data['isbn13'] = dummy_data['isbn13'].astype(int)
-dummy_data['summary']=''
-
-
-# In[73]:
-
-
-API_URL = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
-
-
-# In[74]:
-
-
-df_isbn['isbn'] = df_isbn.head(10)
-df_isbn
-
-
-# In[75]:
-
-
-for row in tqdm(df_isbn):
-    req = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + ""
-    res = urllib.request.urlopen(req)
-    result = json.loads(res.read().decode('utf-8'))
-    print(result['items'][0]['volumeInfo']['description'])
-    
-headers("={"Content-Type":"text"}")
-
-
-# In[76]:
-
-
-books_data= books_data.dropna()
-
-books_data['isbn13'] = books_data['isbn13'].astype(int)
-
-
-# In[77]:
-
-
-for index, row in books_data.iterrows():
-    if row['summary'] == '':
-        print(row['title'])
-        req = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + str(row['isbn13'])
-        res = urllib.request.urlopen(req)
-        result = json.loads(res.read().decode('utf-8'))
-        if 'items' in result:
-            time.sleep(.5)
-            if 'volumeInfo' in result['items'][0]:
-                if 'description' in result['items'][0]['volumeInfo']:
-                    books_data.at[index , 'summary'] = result['items'][0]['volumeInfo']['description']
-                else:
-                    books_data.at[index , 'summary']=np.nan
-            else:
-                books_data.at[index , 'summary']=np.nan
-#                 row['summary']=result['items'][0]['volumeInfo']['description']
-                
-        else:
-            books_data.at[index , 'summary']=np.nan
-
-
-# In[ ]:
-
-
-while True:
-    try:
-        for index, row in books_data.iterrows():
-            if row['summary'] == '':
-#                 print(row['title'])
-                req = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + str(row['isbn13'])
-                res = urllib.request.urlopen(req)
-                result = json.loads(res.read().decode('utf-8'))
-                if 'items' in result:
-#                     time.sleep(1)
-                    if 'volumeInfo' in result['items'][0]:
-                        if 'description' in result['items'][0]['volumeInfo']:
-                            print(row['title'])
-                            books_data.at[index , 'summary'] = result['items'][0]['volumeInfo']['description']
-                        else:
-                            books_data.at[index , 'summary']=np.nan
-                    else:
-                        books_data.at[index , 'summary']=np.nan
-        #                 row['summary']=result['items'][0]['volumeInfo']['description']
-
-                else:
-                    books_data.at[index , 'summary']=np.nan
-    except HTTPError as err:
-        print("stuck")
-
-
-# In[ ]:
-
-
-books_data.to_csv()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-books_data[books_data['summary']==''].shape
-
-
-# In[ ]:
-
-
-books_data[books_data['summary']==NaN].shape
-
-
-# In[ ]:
-
-
-books_data.head(10)
-
-
-# In[ ]:
-
-
-for index, row in dummy_data.iterrows():
-    req = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + str(row['isbn13'])
-#     res = urllib.request.urlopen(req, headers={"Content-Type":"text"})
-    res=requests.get(req, headers={"Content-Type":"text"})
-    print(res)
-
-
-# In[ ]:
-
-
-http = urllib3.PoolManager()
-for index, row in dummy_data.iterrows():
-    time.sleep(.5)
-    req = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + str(row['isbn13'])
-    r = http.request('GET', req)
-    result = r.data.decode("utf-8")
-    print(result)
-
-
-# In[ ]:
-
-
-if 'items' in result:
-    time.sleep(.5)
-    if 'volumeInfo' in result['items'][0]:
-        if 'description' in result['items'][0]['volumeInfo']:
-            books_data.at[index , 'summary'] = result['items'][0]['volumeInfo']['description']
-#                 row['summary']=result['items'][0]['volumeInfo']['description']
-            
-else:
-    books_data.at[index , 'summary']=np.nan
 
