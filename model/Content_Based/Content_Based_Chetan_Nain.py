18e5cd52a13b3ec5653f1b056ef7c905d1cca577
Vamsidhar Reddy Menthem <85726151+vamsidhar18@users.noreply.github.com>
2022-05-23 14:57:25 -0700

    updated_Content_Based_Recommodation_Vamsidhar

--- model/Content_Based/Content_Based_Chetan_Nain.py
+++ model/Content_Based/Content_Based_Chetan_Nain.py
@@ -8,44 +8,44 @@ import pandas as pd
 import numpy as np
 
 
-# In[35]:
+# In[2]:
 
 
 books_data = pd.read_csv('../../data_preprocessing/books_data.csv')
 books_data=books_data.dropna()
 
 
-# In[36]:
+# In[3]:
 
 
 books_data[books_data['authors']=='George Orwell']
 
 
-# In[37]:
+# In[4]:
 
 
 books_data.isnull().sum()
 
 
-# In[38]:
+# In[5]:
 
 
 books_data[books_data['isbn13']==9780451524940]
 
 
-# In[39]:
+# In[6]:
 
 
 books_data.dropna()
 
 
-# In[40]:
+# In[7]:
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-# In[41]:
+# In[8]:
 
 
 tvf = TfidfVectorizer(min_df=3, max_features=None, strip_accents='unicode', analyzer='word', ngram_range=(1,3), token_pattern=r'\w{1,}', stop_words='english')
@@ -57,44 +57,44 @@ tvf = TfidfVectorizer(min_df=3, max_features=None, strip_accents='unicode', anal
 
 
 
-# In[42]:
+# In[9]:
 
 
 tvf_matrix = tvf.fit_transform(books_data['summary'])
 
 
-# In[43]:
+# In[10]:
 
 
 tvf_matrix.shape
 
 
-# In[44]:
+# In[11]:
 
 
 from sklearn.metrics.pairwise import sigmoid_kernel
 sig = sigmoid_kernel(tvf_matrix, tvf_matrix)
 
 
-# In[45]:
+# In[12]:
 
 
 sig[0]
 
 
-# In[46]:
+# In[13]:
 
 
 indices= pd.Series(books_data.index, index=books_data['title']).drop_duplicates()
 
 
-# In[47]:
+# In[14]:
 
 
 indices
 
 
-# In[50]:
+# In[15]:
 
 
 def give_recom(title, sig=sig):
@@ -103,13 +103,18 @@ def give_recom(title, sig=sig):
     sig_score = sorted(sig_score, key=lambda x:x[1], reverse=True)
     sig_score = sig_score[1:11]
     book_indices = [i[0] for i in sig_score]
+    
+    output_file = open('content_op.txt', 'w')
+    output_file.write("\n".join([str(x) for x in book_indices]))
+    output_file.close()
+    
     return books_data['title'].iloc[book_indices]
 
 
-# In[58]:
+# In[16]:
 
 
-give_recom('my sunshine away')
+# give_recom('my sunshine away')
 
 
 # In[ ]:
